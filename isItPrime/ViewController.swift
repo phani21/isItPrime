b8ca0ab671a1e49579591a05b896fdb877a85ea8
Phani_Kiran <pret321@gmail.com>
2019-02-07 13:40:22 -0600

    commit

--- isItPrime/ViewController.swift
+++ isItPrime/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var resultlabel: UILabel!
+    
+    
+    @IBOutlet weak var input: UITextField!
+    @IBAction func isitbutton(_ sender: Any) {
+        print("Button tapped")
+        var i = 1
+        
+        let str = input.text
+       let n = Int(str!)
+       // print(n!)
+        var count=0
+    
+        while (i <= n!) {
+            //print(i)
+            
+            if (n!%i == 0){
+                count+=1
+              //  print("count is \(count)")
+                i+=1
+            }
+            else {i+=1
+            
+            }
+             
+                       if count == 2{
+                resultlabel.text="A prime number"}
+            else{
+                resultlabel.text="Not a prime number"}
+            
+        func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
+
+}
+}
 
 
 }
-
